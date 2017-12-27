fc8f65d1291aaed8d632cbc0430aa0688c318fe9
Payam Mousavi <pmousavi@gmail.com>
2017-12-27 11:22:41 -0800

    AIND projects

---
+++ Project 4 - Recognizer/my_recognizer.py
@@ -0,0 +1,40 @@
+import warnings
+from asl_data import SinglesData
+
+
+def recognize(models: dict, test_set: SinglesData):
+    """ Recognize test word sequences from word models set
+
+   :param models: dict of trained models
+       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
+   :param test_set: SinglesData object
+   :return: (list, list)  as probabilities, guesses
+       both lists are ordered by the test set word_id
+       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            ]
+       guesses is a list of the best guess words ordered by the test set word_id
+           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+   """
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    
+    probs, guesses = [], []
+    for test_word, (X, lengths) in test_set.get_all_Xlengths().items():
+      best_score, best_guess, prob_dict = float("-inf"), "", {}
+      for train_word, model in models.items():
+        try:
+          log_prob = model.score(X, lengths)
+          prob_dict[train_word] = log_prob
+        except:
+          prob_dict[train_word] = float("-inf")
+
+        if log_prob > best_score:
+            best_score = log_prob
+            best_guess = train_word
+
+      probs.append(prob_dict)
+      guesses.append(best_guess)
+
+    return probs, guesses
+    
